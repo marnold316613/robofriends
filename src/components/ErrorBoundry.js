2027f5e929094528d63803d3a8142c999a5a9b33
Mike Arnold <MICHAEL.ARNOLD.316@OUTLOOK.COM>
2024-02-06 20:28:34 -0500

    switch to using hooks in app

---
+++ src/components/ErrorBoundry.js
@@ -0,0 +1,23 @@
+import { render } from "@testing-library/react";
+import React, {Component} from "react";
+
+class ErrorBoundry extends Component {
+    constructor(props) {
+        super()
+        this.state = {
+            hasError: false
+        }
+    }
+    componentDidCatch(error, info) {
+        this.setState({hasError: true})
+    }
+
+
+    render() {
+        if (this.state.hasError) {
+            return <h1>Oops, there was an error</h1>
+        }
+        return this.props.children
+    }
+}
+export default ErrorBoundry;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,44 +1,40 @@
-import React, { Component } from "react";
+import React, {useState,useEffect } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import Scroll from '../components/Scroll';
+import ErrorBoundry from "../components/ErrorBoundry";
 
-class App extends Component {
-    constructor() {
-        super()
-        this.state = {
-            robots: [],
-            searchfield:''
-        } 
-    }
+function App()  {
+    const [searchfield, setSearchField]=useState('');
+    const [robots, setRobots]=useState([]);
 
-    componentDidMount() {
+    useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
         .then(response=> response.json())
-        .then(users => this.setState({robots:users}))
+        .then(users => setRobots(users))
         
-    }
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value}) 
-    }
+    },[])
 
-    render() {
-        const {robots,searchfield} =this.state;
+     const onSearchChange = (event) => {
+        setSearchField(event.target.value) 
+    } 
         const filterRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(searchfield.toLowerCase());
         })
-     return !this.state.robots.length ?
-           <h1>Loading Robots</h1> :  
-        (
+        return !robots.length ?
+            <h1>Loading Robots</h1> :  
+            (
             <div className="tc">
             <h1>RoboFriends</h1>
-            <SearchBox key="searchBox" searchChange={this.onSearchChange} />
+            <SearchBox key="searchBox" searchChange={onSearchChange} />
             <Scroll>
-            <CardList key="cardlist" robots={filterRobots}/>
+                <ErrorBoundry>
+                    <CardList key="cardlist" robots={filterRobots}/>
+                </ErrorBoundry>
             </Scroll>    
             </div>
         );
-    }
+    
 
 }
 export default App;
